8ab21e7cf3f97e9e19fe0e44afd1d680a34d8939
huangjiong@gomo.com <huangjiong@gomo.com>
2025-05-23 14:15:36 +0800

    修复lovCombo不能选择问题

--- src/main/webapp/DBFoundUI/plugin/LovCombo.js
+++ src/main/webapp/DBFoundUI/plugin/LovCombo.js
@@ -113,7 +113,7 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
 				this.store.clearFilter();
 				this.store.each(function(r) {
 					var checked = !(!v.match(
-						 '(^|' + this.separator + ')' + RegExp.escape(r.get(this.valueField))
+						 '(^|' + this.separator + ')' + RegExp.escape(r.get(this.valueField)+"")
 						+'(' + this.separator + '|$)'))
 					;
 
